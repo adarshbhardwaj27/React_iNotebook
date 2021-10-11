3afa02d1697cd01dbe741f0fc55ba7ac860c70f0
Adarsh Bhardwaj <61733737+adarshbhardwaj27@users.noreply.github.com>
2021-10-11 18:26:56 +0530

    DOnt Know what commit  did due to system change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/noteState';
+import Alert from './components/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NoteState>
+    <Router>
+      <Navbar />
+      <Alert message="Hello, to see you" />
+      <div className="container">
+      <Switch>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    </NoteState>
+    </>
   );
 }
 
